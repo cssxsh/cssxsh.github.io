bfc7185f3fd64d68b01f7d838f65ccfdd747acfc
cssxsh <cssxsh@gmail.com>
2020-11-01 22:16:50 +0800

    fix: dns

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 // @ts-ignore
 import doh from 'dohjs'
-import {Button} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 interface answer {
     name: string
@@ -18,42 +18,35 @@ const App: React.FunctionComponent = () => {
     const [result, setResult] = React.useState<string[]>(["# DLSITE"])
     const loadContent = () => {
         setResult(["# DLSITE"])
-        const DohPub = new doh.DohResolver("https://cors.dohjs.workers.dev/https://doh.pub/dns-query")
-        const Cloudflare = new doh.DohResolver("https://cloudflare-dns.com/dns-query")
+        const DohPub = new doh.DohResolver("https://doh.pub/dns-query")
+        const Cloudflare = new doh.DohResolver("https://1.0.0.1/dns-query")
 
-        const host_1 = ["trial.dlsite.com", "img.dlsite.jp", "media.dlsite.com", "play.dl.dlsite.com", "media.ci-en.jp"
-            , "media.stg.ci-en.jp", "www.nijiyome.com", "www.nijiyome.jp", "file.chobit.cc", "img.chobit.cc"]
-        const host_2 = ["www.dlsite.com", "ssl.dlsite.com", "download.dlsite.com"]
-        const host_3 = "play.dlsite.com"
-        const host_4 = "login.dlsite.com"
-
-        const hostMap = new Map<string, string[] | string>([
-            ["download.sangfor.com.cn", host_1],
-            ["w2.shared.global.fastly.net", host_2],
-            ["play.dlsite.com", host_3],
-            ["login.dlsite.com", host_4]
+        const hostMap = new Map<string, [any, string[]]>([
+            ["download.sangfor.com.cn", [DohPub, [
+                "trial.dlsite.com", "img.dlsite.jp", "media.dlsite.com", "play.dl.dlsite.com", "media.ci-en.jp",
+                "media.stg.ci-en.jp", "file.chobit.cc", "img.chobit.cc"
+            ]]],
+            ["j.sni.global.fastly.net", [Cloudflare, [
+                "ssl.dlsite.com", "www.dlsite.com", "www.nijiyome.com", "www.nijiyome.jp"
+            ]]],
+            ["play.dlsite.com", [Cloudflare, [
+                "play.dlsite.com"
+            ]]],
+            ["login.dlsite.com", [Cloudflare, [
+                "login.dlsite.com"
+            ]]],
+            ["download.dlsite.com.wtxcdn.com", [DohPub, [
+                "download.dlsite.com"
+            ]]]
         ])
 
-        hostMap.forEach((value, key) => {
-            if (typeof value === "string") {
-                Cloudflare.query(key, "A")
-                    .then((response: { answers: answer[]; }) => {
-                        const ip_addr = response.answers
-                            .filter(ans => ans.type === "A")
-                            .map(ans => ans.data)
-                        const list = [`# ${key}`].concat(ip_addr.map(ip => `${ip} ${value} `))
-                        setResult(prevState => prevState.concat(list))
-                    })
-            } else {
-                DohPub.query(key, "A")
-                    .then((response: { answers: answer[]; }) => {
-                        const ip_addr = response.answers
-                            .filter(ans => ans.type === "A")
-                            .map(ans => ans.data)
-                        const list = [`# ${key}`].concat(value.flatMap(host => ip_addr.map(ip => `${ip} ${host} `)))
-                        setResult(prevState => prevState.concat(list))
-                    })
-            }
+        hostMap.forEach(([dns, hosts], cname) => {
+            dns.query(cname, "A", "GET").then((response: { answers: answer[]; }) => {
+                const ipAddr = response.answers.filter(ans => ans.type === "A").map(ans => ans.data)
+                const list = [`## ${cname}`].concat(hosts.flatMap(host => ipAddr.map(ip => `${ip}\t\t${host} `)))
+                setResult(prevState => prevState.concat(list))
+                console.log(`${list}加载成功`)
+            })
         })
     }
 
@@ -62,7 +55,8 @@ const App: React.FunctionComponent = () => {
     return (
         <React.Fragment>
             <CssBaseline />
-            <Button onClick={loadContent}>LOAD</Button>
+            <Button onClick={loadContent} color="primary">刷新</Button>
+            <h3>使用前请将已有的旧HOSTS清除，火狐加载可能存在问题，请尝试Window自带的Edge</h3>
             <Paper>
                 {result.map(line => (<li key={line}>{line}</li>))}
             </Paper>
