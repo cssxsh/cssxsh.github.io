fab7b31cf4ef64d89f21310b508547c9df55160d
cssxsh <cssxsh@gmail.com>
2020-11-01 22:16:08 +0800

    add: corsProxy

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import corsProxy from './corsProxy';
-
-corsProxy()
 
 ReactDOM.render(
   <React.StrictMode>
