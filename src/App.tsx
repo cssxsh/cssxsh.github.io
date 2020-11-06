737c80c4bd6a6bddf2dcd449174995004760375a
cssxsh <cssxsh@gmail.com>
2020-11-07 01:28:44 +0800

    add: dns list

--- src/App.tsx
+++ src/App.tsx
@@ -14,51 +14,82 @@ interface answer {
     data: string
 }
 
+
+const DohPub = new doh.DohResolver("https://doh.pub/dns-query")
+const AliDns = new doh.DohResolver("https://dns.alidns.com/dns-query")
+
+const ChinaDns = [
+    DohPub,
+    AliDns
+]
+
+const Cloudflare = new doh.DohResolver("https://cloudflare-dns.com/dns-query")
+const OneOneOneOne = new doh.DohResolver("https://1.0.0.1/dns-query")
+// const CleanBrowsing = new doh.DohResolver("https://doh.cleanbrowsing.org/doh/security-filter/")
+// const Comss = new doh.DohResolver("https://dns.east.comss.one/dns-query")
+const BlahDns = new doh.DohResolver("https://doh-jp.blahdns.com/dns-query")
+// const Snopyta = new doh.DohResolver("https://fi.doh.dns.snopyta.org/dns-query")
+// const Twnic = new doh.DohResolver("https://dns.twnic.tw/dns-query")
+/// const Oszx = new doh.DohResolver("https://dns.oszx.co/dns-query")
+/// const DohSb = new doh.DohResolver("https://doh.dns.sb/dns-query")
+// const DnsForge = new doh.DohResolver("https://dnsforge.de/dns-query")
+const Eastas = new doh.DohResolver("https://doh.eastas.pi-dns.com/dns-query")
+
+const OtherDns = [
+    Cloudflare,
+    OneOneOneOne,
+    BlahDns,
+    Eastas
+]
+
+
+const hostMap = new Map<string, [boolean, string[]]>([
+    ["download.sangfor.com.cn", [false, [
+        "trial.dlsite.com", "img.dlsite.jp", "media.dlsite.com", "play.dl.dlsite.com", "media.ci-en.jp",
+        "media.stg.ci-en.jp", "file.chobit.cc", "img.chobit.cc"
+    ]]],
+    ["download.dlsite.com.wtxcdn.com", [false, [
+        "download.dlsite.com"
+    ]]],
+    ["j.sni.global.fastly.net", [true, [
+        "ssl.dlsite.com", "www.dlsite.com", "www.nijiyome.com", "www.nijiyome.jp", "download.dlsite.com"
+    ]]],
+    ["play.dlsite.com", [true, [
+        "play.dlsite.com"
+    ]]],
+    ["login.dlsite.com", [true, [
+        "login.dlsite.com"
+    ]]]
+])
+
 const App: React.FunctionComponent = () => {
-    const [result, setResult] = React.useState<string[]>(["# DLSITE"])
+    const [result, setResult] = React.useState<Map<string, string[]>>(new Map())
+    const [load, setLoad] = React.useState<number>(0)
     const loadContent = () => {
-        setResult(["# DLSITE"])
-        const DohPub = new doh.DohResolver("https://doh.pub/dns-query")
-        const Cloudflare = new doh.DohResolver("https://1.0.0.1/dns-query")
+        setResult(new Map())
 
-        const hostMap = new Map<string, [any, string[]]>([
-            ["download.sangfor.com.cn", [DohPub, [
-                "trial.dlsite.com", "img.dlsite.jp", "media.dlsite.com", "play.dl.dlsite.com", "media.ci-en.jp",
-                "media.stg.ci-en.jp", "file.chobit.cc", "img.chobit.cc"
-            ]]],
-            ["j.sni.global.fastly.net", [Cloudflare, [
-                "ssl.dlsite.com", "www.dlsite.com", "www.nijiyome.com", "www.nijiyome.jp"
-            ]]],
-            ["play.dlsite.com", [Cloudflare, [
-                "play.dlsite.com"
-            ]]],
-            ["login.dlsite.com", [Cloudflare, [
-                "login.dlsite.com"
-            ]]],
-            ["download.dlsite.com.wtxcdn.com", [DohPub, [
-                "download.dlsite.com"
-            ]]]
-        ])
-
-        hostMap.forEach(([dns, hosts], cname) => {
-            dns.query(cname, "A", "GET").then((response: { answers: answer[]; }) => {
-                const ipAddr = response.answers.filter(ans => ans.type === "A").map(ans => ans.data)
-                const list = [`## ${cname}`].concat(hosts.flatMap(host => ipAddr.map(ip => `${ip}\t\t${host} `)))
-                setResult(prevState => prevState.concat(list))
-                console.log(`${list}加载成功`)
+        hostMap.forEach(([isWall, hosts], cname) => {
+            (isWall ? OtherDns : ChinaDns).forEach((dns) => {
+                dns.query(cname, "A", "GET").then((response: { answers: answer[]; }) => {
+                    const ipAddr = response.answers.filter(ans => ans.type === "A").map(ans => ans.data)
+                    const list = hosts.flatMap(host => ipAddr.map(ip => `${ip} ${host} `))
+                    setResult(prevState => prevState.set(cname, (prevState.get(cname) || []).concat(list)))
+                    setLoad(prevState => prevState + 1)
+                })
             })
         })
     }
-
     React.useEffect(loadContent, [])
-
+    const lines = ["# DLSite"].concat(Array.from(result.entries()).flatMap(([cname, hosts]) => {
+        return [`## ${cname}`].concat(Array.from(new Set(hosts)))
+    }))
     return (
         <React.Fragment>
             <CssBaseline />
             <Button onClick={loadContent} color="primary">刷新</Button>
-            <h3>使用前请将已有的旧HOSTS清除，火狐加载可能存在问题，请尝试Window自带的Edge</h3>
+            <h3>使用前请将已有的旧HOSTS清除，火狐加载可能存在问题，请尝试使用谷歌内核的浏览器访问。</h3>
             <Paper>
-                {result.map(line => (<li key={line}>{line}</li>))}
+                {lines.map(line => (<li key={line}>{line}</li>))}
             </Paper>
         </React.Fragment>
     );
