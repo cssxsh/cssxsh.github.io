354b3b7d6690c53eb6c7b3f9e5da4babc4b86fb3
cssxsh <cssxsh@gmail.com>
2021-04-04 21:44:52 +0800

    add: saveContent

--- src/App.tsx
+++ src/App.tsx
@@ -14,34 +14,27 @@ interface answer {
     data: string
 }
 
-
-const DohPub = new doh.DohResolver("https://doh.pub/dns-query")
-const AliDns = new doh.DohResolver("https://dns.alidns.com/dns-query")
-
 const ChinaDns = [
-    DohPub,
-    AliDns
+    new doh.DohResolver("https://doh.pub/dns-query"),
+    new doh.DohResolver("https://dns.alidns.com/dns-query")
 ]
 
-const Cloudflare = new doh.DohResolver("https://cloudflare-dns.com/dns-query")
-const OneOneOneOne = new doh.DohResolver("https://1.0.0.1/dns-query")
 // const CleanBrowsing = new doh.DohResolver("https://doh.cleanbrowsing.org/doh/security-filter/")
 // const Comss = new doh.DohResolver("https://dns.east.comss.one/dns-query")
-const BlahDns = new doh.DohResolver("https://doh-jp.blahdns.com/dns-query")
 // const Snopyta = new doh.DohResolver("https://fi.doh.dns.snopyta.org/dns-query")
-// const Twnic = new doh.DohResolver("https://dns.twnic.tw/dns-query")
 /// const Oszx = new doh.DohResolver("https://dns.oszx.co/dns-query")
 /// const DohSb = new doh.DohResolver("https://doh.dns.sb/dns-query")
 // const DnsForge = new doh.DohResolver("https://dnsforge.de/dns-query")
-const Eastas = new doh.DohResolver("https://doh.eastas.pi-dns.com/dns-query")
 
 const OtherDns = [
-    Cloudflare,
-    OneOneOneOne,
-    BlahDns,
-    Eastas
+    new doh.DohResolver("https://dns.233py.com/dns-query"),
+    new doh.DohResolver("https://dns.google/dns-query"),
+    new doh.DohResolver("https://dns.twnic.tw/dns-query"),
+    new doh.DohResolver("https://cloudflare-dns.com/dns-query"),
+    new doh.DohResolver("https://1.0.0.1/dns-query"),
+    // new doh.DohResolver("https://doh-jp.blahdns.com/dns-query"),
+    // new doh.DohResolver("https://doh.eastas.pi-dns.com/dns-query")
 ]
-
 
 const hostMap = new Map<string, [boolean, string[]]>([
     ["download.sangfor.com.cn", [false, [
@@ -62,6 +55,21 @@ const hostMap = new Map<string, [boolean, string[]]>([
     ]]]
 ])
 
+const download = (filename: string, text: string) => {
+    const pom = document.createElement('a');
+    pom.setAttribute('href', 'data:application/octet-stream,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        const event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
 const App: React.FunctionComponent = () => {
     const [result, setResult] = React.useState<Map<string, string[]>>(new Map())
     const [load, setLoad] = React.useState<number>(0)
@@ -79,6 +87,11 @@ const App: React.FunctionComponent = () => {
             })
         })
     }
+    const saveContent = () => {
+        const hosts = lines.join("\n");
+        const path = "hosts"
+        download(path, hosts);
+    }
     React.useEffect(loadContent, [])
     const lines = ["# DLSite"].concat(Array.from(result.entries()).flatMap(([cname, hosts]) => {
         return [`## ${cname}`].concat(Array.from(new Set(hosts)))
@@ -87,7 +100,9 @@ const App: React.FunctionComponent = () => {
         <React.Fragment>
             <CssBaseline />
             <Button onClick={loadContent} color="primary">刷新</Button>
+            <Button onClick={saveContent} color="primary">下载</Button>
             <h3>使用前请将已有的旧HOSTS清除，火狐加载可能存在问题，请尝试使用谷歌内核的浏览器访问。</h3>
+            <h3>请保存Hosts至路径覆盖原有文件 C:\Windows\System32\drivers\etc\hosts （最好先备份）</h3>
             <Paper>
                 {lines.map(line => (<li key={line}>{line}</li>))}
             </Paper>
